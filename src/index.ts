5f2da9a4394d6b936430c8848b61a9553a667804
Ongsalt <ongsa.srp.mkm@gmail.com>
2024-11-24 14:07:50 +0700

    feat: 50% chance to use claude instead of chatgpt

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { OpenAIClient, ClaudeClient, type RequestHandler } from "./clients"
 
-const client: RequestHandler = new OpenAIClient()
+const client: RequestHandler = new ClaudeClient()
+const client2: RequestHandler = new OpenAIClient()
 
 function start() {
     console.log(`Starting server at port ${Bun.env.PORT ?? 3000}`)
     Bun.serve({
-        fetch: async (req) => client.handleRequest(req),
+        fetch: async (req) => {
+            if (Math.random() > 0.5) {
+                return await client.handleRequest(req)
+            } else {
+                return await client2.handleRequest(req)
+            }
+        },
         port: Bun.env.PORT ?? 3000,
         idleTimeout: 255,
     })
